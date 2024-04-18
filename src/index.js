7fa4b315c30d0082df6e70c94d3f87fadf099ccc
yevhenii-bezeha <bezegajenja0305@gmail.com>
2024-04-18 12:27:47 +0200

    Add deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { GlobalStyles } from './styles/GlobalStyles';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/test-task-car-rent">
+  <BrowserRouter basename="/">
     <Provider store={store}>
       <App />
       <GlobalStyles />
